b24c27f281f4d6a13d03c79ccba5556ba7dbdca5
singhguri <61290735+singhguri@users.noreply.github.com>
2021-11-25 14:15:29 +0530

    comment use of mongo db for the time being

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("./db/db");
+// require("./db/db");
 const Product = require("./models/products");
 
 const app = express();
